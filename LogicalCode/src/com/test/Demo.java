9c44532d10c78060650e4b24529d926fcabe0257
mohit <mk82058104@gmail.com>
2022-09-04 17:44:55 +0530

    adding scnner logic

--- LogicalCode/src/com/test/Demo.java
+++ LogicalCode/src/com/test/Demo.java
@@ -1,9 +1,20 @@
 package com.test;
 
+import java.util.Scanner;
+
 public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Hi");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your name");
+		String str=sc.nextLine();
+		if(str.equals("yes")) {
+			System.out.println("I am good");
+		}
+		else {
+			System.out.println("I am bad");
+		}
 
 	}
 
